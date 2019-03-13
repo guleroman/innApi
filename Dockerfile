fd851e59d79e462e65142f2487b02829cad22d79
guleroman <remembername@protonmail.com>
2019-03-13 21:55:44 +0300

    'init'

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,14 @@
 FROM python:3-onbuild
-#RUN pip3 install --upgrade pip
-RUN pip3 install requests bs4
+RUN pip3 install --upgrade pip
 
 RUN git clone https://github.com/guleroman/innApi.git /API
 
 WORKDIR /API
 
-#ARG inn
 
-EXPOSE 8888
-
+EXPOSE 5000
 
 
 
-ENTRYPOINT ["python3", "innApi.py"]
+
+ENTRYPOINT ["python3", "app.py"]
--- app.py
+++ app.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
 @app.route('/api/', methods=['GET'])
-def get_tasks():
+def main():
 	inn=request.args.get('inn')
 	data = innApi.mainn(inn)
 	#print (data)
 	return jsonify({'tasks': data})
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=True ,host='0.0.0.0', port=5000)
